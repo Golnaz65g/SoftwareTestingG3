eeb396e0ff2056e22c6842f4b424cee8beee64c3
unknown <mdna19@student.bth.se>
2020-04-18 22:01:21 +0200

    Initial commit with extended calculator project

---
+++ src/main/java/G3/ConCat.java
@@ -0,0 +1,7 @@
+package G3;
+
+public class ConCat {
+	public String ConCating(String a, String b) {
+		return a+b ;
+	}
+}
---
+++ src/test/java/G3/ConCatTest.java
@@ -0,0 +1,16 @@
+package G3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ConCatTest {
+
+	@Test
+	public void ConCatingTest() {
+		ConCat junit= new ConCat();
+		String result= junit.ConCating("Hello", "World");
+		assertEquals ("HelloWorld", result);		
+	}
+
+}
